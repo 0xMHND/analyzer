46a11dde2c32e2b2d5996df324d6efb031c2c037
Luke Morrison <morriluk@oregonstate.edu>
2015-11-29 17:37:24 -0800

    Commented code and added error handling in .py

--- add_prints/add_prints.py
+++ add_prints/add_prints.py
@@ -9,26 +9,44 @@ DbDestroy_substr = "\\1/*Auto-Generated Print:*/PRINTF(\"DbDestroy: %p\\\\n\",\\
 DbRelease_pattern = re.compile(r"(ocrDbRelease\(([^;]+)\)[^ \t\n\r\f\v]*;)(?!\/\*Auto-Generated Print:\*\/)",re.MULTILINE)
 DbRelease_substr = "\\1/*Auto-Generated Print:*/PRINTF(\"DbRelease: %p\\\\n\",\\2);"
 
+
 def main(filename):
+	#Open filename into string s
+	try:
+		f_read = open(filename, "r")
+		s = f_read.read(-1)
+		f_read.close();
+	except IOError as e:
+		print("I/O read error({0}): {1}".format(e.errno, e.strerror))
+	except:
+		print("Unexpected error:", sys.exc_info()[0])
+		raise
 
-	f_read = open(filename, "r")
-	s = f_read.read(-1)
-	f_read.close();
-
+	#Add print statements after each ocrDb function call
 	s = DbCreate_pattern.sub(DbCreate_substr, s)
 	s = DbDestroy_pattern.sub(DbDestroy_substr, s)
 	s = DbRelease_pattern.sub(DbRelease_substr, s)
 
-	f_write = open(filename, "w")
-	f_write.write(s)
-	f_write.close();
+	#Open filename and write string s to it
+	try:
+		f_write = open(filename, "w")
+		f_write.write(s)
+		f_write.close();
+	except IOError as e:
+		print("I/O write error({0}): {1}".format(e.errno, e.strerror))
+	except:
+		print("Unexpected error:", sys.exc_info()[0])
+		raise
 
+	#done
 	print("Done.")
 	
 
-
+#start here
 if __name__=="__main__":
+	#Check that there is an entered filename
 	if len(sys.argv) == 2: main(sys.argv[1]);
+	#Otherwise print instructions and exit
 	else:
 		print("Usage: python3 ", sys.argv[0], " <filename>")
 		sys.exit(1)
