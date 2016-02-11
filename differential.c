2ba1ab3e9bbd1b61c717d7cee1110eb340c85354
Luke Morrison <lukemorrison@hotmail.fr>
2016-02-11 00:35:48 -0800

    update block differential

--- differential.c
+++ differential.c
@@ -7,48 +7,75 @@
 #include <dirent.h>
 #include <pwd.h>
 #include <unistd.h>//for access function
+#include <errno.h>
+#include <sys/types.h>
+
 
 #define NAME_SIZE 32
-#define LOG_PATH "\0"
+#define LOGS_PATH "../"
 #define SIZE 64
 
-//header
-char *get_file_name(int index, char* dir_name, int file_num);
-char* create_diff_command(char* name1, char *dir1, char *name2, char* dir2);
-void get_dir_name(char* first_dir, char* second_dir);
+volatile int VERBOSE = 0;
 
+/***************************
+ * input: index of which file, dir name, number of files
+ * output: one file name
+ * description:	get file names from a dir
+***************************/
+char *get_file_name(int index, char* dir_name, int file_num);
+
+
+/***************************
+ * input: input from keyboard
+ * output: one dir name
+ * description:	get dir name from user
+***************************/
+char* get_dir_name_1(char* first_dir);
+
+
+/***************************
+ * input: 2 names of files and 2 names of dirs
+ * output: command fed in differ file
+ * description:	generate command instruction
+***************************/
+char* create_diff_command(char* name1, char *dir1, char *name2, char* dir2);
+
+
+/***************************
+ * input: name of a dir
+ * output: number of files in the dir
+ * description:	count the number of files in a dir
+***************************/
+int get_file_num(char* dir_name);
+
+
+/***************************
+ * input: name of 2 dirs
+ * output: name of the file out logs in
+ * description:	it combine 2 dir names and generate a new dir name
+***************************/
+char *get_save_dir_name(char *dir1, char *dir2);
+
+//example error output
+/*
 void example(){
 	if (dir == NULL)
 		fprintf(stderr, "Cannot open dir %s: %s",dir_name ,strerror(errno));
 	exit(1);
 }
-
-
-//create file function
-void create_files(){
-	int i,j;
-	char *file_name = malloc(NAME_SIZE*sizeof(char));
-	char *dir_name = malloc(NAME_SIZE*sizeof(char));
-	//make a dir
-	file_name = "test_dir";
-	file_name = "test_file";
-	mkdir(dir_name, 0777);
-	chdir(dir_name);
-	FILE *fp = fopen(file_name, "w");
-	//loop:
-	fprintf(fp, "hello world");
-	//end of loop:
-	fclose(fp);
-	chdir("..");
-//	free(dir_name);
-//	free(file_name);
+*/
+char *get_save_dir_name(char *dir1, char *dir2) {
+	//length of my dir name
+	char *name = malloc(SIZE * sizeof(char));
+	//format is compare.logs.dir1_and_dir2
+	sprintf(name, "compare.logs.%s_and_%s", dir1, dir2);
+	return name;
 }
 
-//create command for popen function
 char* get_dir_name_1(char* first_dir){
 	int	flag = 1;
 	//names of dir
-	first_dir = malloc(32*sizeof(char));
+	first_dir = malloc(SIZE*sizeof(char));
 	//to pass in get_name function 
 	//input 1
 	while (flag == 1){
@@ -81,33 +108,51 @@ char* create_diff_command(char* name1, char *dir1, char *name2, char* dir2){
 
 	//create the command to fed in system	
 	char* command = malloc(128*sizeof(char));
-	strcpy(command, "diff ");
-//	strcat(command, LOG_PATH);
+	strcpy(command, "diff -u ");
+	strcat(command, LOGS_PATH);
 	strcat(command, dir1);
 	strcat(command, "/");
 	strcat(command, name1);
 	strcat(command, " ");
-//	strcat(command, LOG_PATH);
+	strcat(command, LOGS_PATH);
 	strcat(command, dir2);
 	strcat(command, "/");
 	strcat(command, name2);
-	strcat(command, " > comparing_");
-	strcat(command, name1);
-	strcat(command, "_and_");
-	strcat(command, name2);
-	//char buffer[2];
-	//snprintf(buffer, 2, "%d", index);
-	//strcat(command, buffer);
-	strcat(command, ".txt\0");
+	printf("The default file name is: comparing %s_and_%s\n", name1, name2);
+	char input[40];
+	printf("Input yes to continue, input new file name to change.\n");
+	scanf("%s", input);
+	//if input is yes, use the default name
+	if (!strcmp(input, "yes")){
+		strcat(command, " > comparing_");
+		strcat(command, name1);
+		strcat(command, "_and_");
+		strcat(command, name2);
+		strcat(command, ".txt\0");
+	}
+	//else use the input as name
+	else {
+		strcat(command, " > ");
+		strcat(command, input);
+		strcat(command, "\0");
+	}
+	if (VERBOSE == 1)
+		printf ("Program running:\n%s\n", command);
 	return command;
 }
 
 char* get_file_name(int index, char* dir_name, int file_num){
  	int i;
-	chdir(dir_name);	
-	DIR *dp;
+	DIR *dp;	
 	struct dirent *dir;
+	//2d array for the names of files
 	char name_array[file_num][SIZE];
+	//error check for open a dir
+	if (chdir(dir_name)== -1){
+		fprintf(stderr, "Cannot change to dir %s: %s\n",dir_name ,strerror(errno));
+		printf("1\n");
+		exit(1);
+	}
 	dp = opendir (".");
 	i = 0;
 	while ((dir = readdir (dp))!= NULL) {
@@ -118,6 +163,7 @@ char* get_file_name(int index, char* dir_name, int file_num){
 		}
 	closedir(dp);
 	chdir("..");
+	//return the ith of the file
 	char *name = malloc(SIZE*sizeof(char));
 	strcpy(name, name_array[index]);
 	return name;
@@ -125,10 +171,15 @@ char* get_file_name(int index, char* dir_name, int file_num){
 
 int get_file_num(char* dir_name){
 	int count = 0;
-	chdir(dir_name);
+	if (chdir(dir_name)== -1){
+		fprintf(stderr, "Cannot change to dir %s: %s\n",dir_name ,strerror(errno));
+		printf("2\n");
+		exit(1);
+	}	
 	DIR *dp;
 	struct dirent *dir;
 	dp = opendir(".");
+	//count the files
 	while ((dir = readdir (dp))!= NULL) {
 		if (strcmp(dir->d_name, ".") && strcmp(dir->d_name, "..")){
 			count++;	
@@ -139,16 +190,44 @@ int get_file_num(char* dir_name){
 	return count;
 }
 
-int main(){
+int main(int argc, char ** argv){
+
+	char buffer[10];
 	int i,j,status;
 	//fet dir name
-	char *dir1;
-	char *dir2;
+	char *dir1=NULL;
+	char *dir2=NULL;
+	//error checking input
+	if (argc > 2) {
+		printf("Usage: %s [-v]\n", argv[0]);
+		exit(1);
+	}
+	// -v represent for verbose setting
+	else if (argc == 2) {
+		if (!strcmp(argv[1],"-v"))
+			VERBOSE = 1;
+		else {
+			printf("Invalid Argument. Usage %s [-v]\n", argv[0]);
+			return 1;
+		}
+	}
+	//get fir names
 	dir1 = get_dir_name_1(dir1);
 	dir2 = get_dir_name_2(dir2);
+	//open files
+	char *save_dir_name = get_save_dir_name(dir1, dir2);
+	mkdir(save_dir_name, 0777);
+	if (VERBOSE == 1){
+		printf("The first dir been chosen is:  %s\n", dir1);
+		printf("The second dir been chosen is: %s\n", dir2);
+	}
 	//get file numbers
 	int file1_num = get_file_num(dir1);
 	int file2_num = get_file_num(dir2);
+	if (VERBOSE == 1){
+		printf("%d files in dir1.\n", file1_num);
+		printf("%d diles in dir2.\n", file2_num);
+	}
 	//get file name
 	char file1_names[file1_num][SIZE];
 	char file2_names[file2_num][SIZE];
@@ -156,22 +235,34 @@ int main(){
 		strcpy (file1_names[i], get_file_name(i, dir1, file1_num));	
 	for (i = 0; i < file2_num; i++)
 		strcpy (file2_names[i], get_file_name(i, dir2, file2_num));
-
-
 	//get command
 	char *command = malloc(128*sizeof(char));
-	//loop
+	//loop to find the same names of files and compare them, 2 files and a time
 	for (i = 0; i < file1_num; i++){
 		for (j = 0; j < file2_num; j++){
 			if (strcmp(file1_names[i],file2_names[j]) == 0){
 				strcpy(command, create_diff_command(file1_names[i], dir1, file2_names[j], dir2));
-				printf("command is: %s|\n",command);//debug
-				status = system(command);	
+				
+				printf("The file comparing are %s and %s.\n", file1_names[i], file2_names[j]);
+				printf("Enter yes to compare, input no to cancel the compare.\n");
+				scanf("%s", buffer);
+				if(!strcmp(buffer, "yes")){
+					chdir(save_dir_name);
+					status = system(command);
+					chdir("..");
+					if (status == -1){
+						fprintf(stderr, "Cannot run this command %s: %s\n",command ,strerror(errno));
+						exit(1);
+					}
+				}				
 			}
 		}
 	}
 	free(command);
 	free(dir2);
-	free(dir1);	
+	free(dir1);
+	free(save_dir_name);
+	if (VERBOSE == 1)
+		printf("program done, exit 0.\n");	
 	return 0;
 }
